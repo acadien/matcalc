9d57ed7ea39a213509a0db9850d2e618f0c319c3
Adam Cadien <adamcadien@gmail.com>
2012-03-28 11:11:52 -0400

    here goes
     Please enter the commit message for your changes. Lines starting

--- analysis/datatools.py
+++ analysis/datatools.py
@@ -7,8 +7,9 @@ from numpy import convolve
 import pylab as pl
 from itertools import chain
 #mine
-from functions import *
+from mathfunctions import *
 
+#1D data
 def gaussSmooth(a,n=9,mode='same'):
     if n%2==0:
         n+=1
@@ -16,6 +17,7 @@ def gaussSmooth(a,n=9,mode='same'):
     gWeight=[gaussNorm1D(float(i),center,float(1)) for i in range(n)]
     return convolve(a,gWeight,mode=mode)#[center-1:-center-1]
 
+#1D data
 def windowAvg(a,n=10):
     #a: the list/array to run the window average over
     #n: the size of the window
@@ -31,35 +33,7 @@ def windowAvg(a,n=10):
         winavg.append(sum(window)/len(window))
     return winavg
 
-def FIR(n,wl,wh):
-    return (sin(wh*n) - sin(wl*n))/pi/n
-
-def bandpass(x,y,bandlow,bandhigh):
-    #x/y: the lists/arrays to perform the bandpass on, assumes evenly spaced
-    #bandlow/bandhigh: the frequency range to work in
-
-    N=len(y)
-    dx=x[1]-x[0]
-
-    y=[sin(i) for i in x]
-    wl=2*dx*bandlow*pi
-    wh=2*dx*bandhigh*pi
-
-    M=N
-    bn = [(wh-wl)/pi]
-    bn += [FIR(i-M/2,wl,wh) for i in range(1,M)]
-    bn = array(bn)
-    bn = bn*kaiser(M,5.2)
-    
-
-    [w,h]=freqz(bn,1)
-
-    filtered=convolve(bn,array(y))
-    pl.plot(y)
-    pl.plot(filtered)
-    pl.show()
-    exit()
-
+#A more advanced smoothing algorithm than windowed average
 def wsmooth(x,window_len=11,window='hanning'):
     if type(x) != type(array(1)):
         x=array(x)
@@ -89,6 +63,37 @@ def wsmooth(x,window_len=11,window='hanning'):
     y=convolve(w/w.sum(),s,mode='valid')
     M=window_len/2
     return y[M:-M+1]
+
+#Finite Impulse Response
+def FIR(n,wl,wh):
+    return (sin(wh*n) - sin(wl*n))/pi/n
+
+def bandpass(x,y,bandlow,bandhigh):
+    #x/y: the lists/arrays to perform the bandpass on, assumes evenly spaced
+    #bandlow/bandhigh: the frequency range to work in
+
+    N=len(y)
+    dx=x[1]-x[0]
+
+    y=[sin(i) for i in x]
+    wl=2*dx*bandlow*pi
+    wh=2*dx*bandhigh*pi
+
+    M=N
+    bn = [(wh-wl)/pi]
+    bn += [FIR(i-M/2,wl,wh) for i in range(1,M)]
+    bn = array(bn)
+    bn = bn*kaiser(M,5.2)
+    
+
+    [w,h]=freqz(bn,1)
+
+    filtered=convolve(bn,array(y))
+    pl.plot(y)
+    pl.plot(filtered)
+    pl.show()
+    exit()
+
 
 #Takes a 2D list and turns it into a 1D list
 def flatten(listOfLists):
---
+++ analysis/fieldPointAnalysis.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+
+import sys,operator
+from scipy import *
+#mine
+from struct_tools import dist_periodic
+from interpolate import interp3d
+
+#Returns True if any point in lgrids is out of 'bounds'
+def outOfBounds(lgrids,bounds):
+    for lgrid in lgrids:
+        for i in range(3):
+            if lgrid[i][1]>bounds[i]:
+                return True
+            elif lgrid[i][0]<0:
+                return True
+    return False
+
+#Returns the index of the point that is out of bounds
+def whichBounds(lgrids,bounds):
+    for a,lgrid in enumerate(lgrids):
+        for i in range(3):
+            if lgrid[i][1]>bounds[i]:
+                return a
+            elif lgrid[i][0]<0:
+                return a
+
+#Creates a 3D line from 2 points
+def linePoints(a,b,N): 
+    eqn=lambda t: a+t*(b-a)
+    return [eqn(float(i)/N) for i in range(N)]
+
+#Loop over neighbors provided (otherwise use vornoiNeighbors)
+#Interpolate the 3D Field between each of these neighbors
+#Note: it is much more efficient to do the bondcutoff comparision within the grid loop, as weird as it may be to work with.
+def fieldNeighbors(atoms,atypes,basis,field,gridSize,halfNeighbors=None,Ninterp=50):
+    [v1,v2,v3]=basis
+    latoms=array(atoms)
+
+    #Ensure simulation box is orthorhombic
+    if sum(map(fabs,v1))-fabs(v1[0]) > 1e-10 or \
+       sum(map(fabs,v2))-fabs(v2[1]) > 1e-10 or \
+       sum(map(fabs,v3))-fabs(v3[2]) > 1e-10:
+        print "Error: simulation axes are not sufficiently orthogonal.  Use script poscarRectify.py and regenerate doscar"
+        exit(0)
+    
+    bounds=[[0.,v1[0]],[0.,v2[1]],[0.,v3[2]]]
+    simSize=asarray([v1[0],v2[1],v3[2]])
+
+    if halfNeighbors==None:
+        halfNeighbors=voronoiNeighbors(atoms=latoms,basis=basis,atypes=atypes,style='half')
+
+    #Interpolation and summation properties
+        #avgfields=zeros([len(bondCutoffs),Ninterp])
+        #avgfails=zeros([len(bondCutoffs),Ninterp])
+        #naes=zeros(len(bondCutoffs))
+        #nafs=zeros(len(bondCutoffs))
+    lines=list()
+    avgline=zeros(Ninterp)
+    nlines=float(sum(map(len,halfNeighbors)))
+
+    #Local Grid size
+    delGrids=simSize/gridSize
+    lGridSize=gridSize
+    nlGridPoints=reduce(operator.mul,lGridSize)
+    for iNeighb,jNeighbors in enumerate(halfNeighbors):
+        lines.append(list())
+        if len(jNeighbors)==0:
+            continue
+
+        atomi=latoms[iNeighb]
+        for i,ai in enumerate(atomi):
+            if ai < 0.0:
+                atomi[i]+=simSize[i]
+            if ai > simSize[i]:
+                atomi[i]-=simSize[i]
+
+        lGridCenter = map(int,atomi/simSize*gridSize)
+        lGridBounds = [[lGridCenter[i]-lGridSize[i]/2,lGridCenter[i]+lGridSize[i]/2] for i in range(3)]
+        lGridBoundPoints = array([[lGridBounds[i][0]*delGrids[i],lGridBounds[i][1]*delGrids[i]] for i in range(3)])
+        lGridPoints = array([map(lambda x: x*delGrids[i],range(lGridCenter[i]-lGridSize[i]/2,lGridCenter[i]+lGridSize[i]/2)) for i in range(3)])
+
+        #Stitch together field in local field (lfield) to enforce periodicity, use bounds to simplify
+        bnds=list()
+        lbnds=list()
+        bnds.append(list(lGridBounds))
+        lbnds.append(map(list,zip([0,0,0],lGridSize)))
+        while outOfBounds(bnds,gridSize):
+            a=whichBounds(bnds,gridSize)
+            bnds.append(map(list,bnds[a]))
+            lbnds.append(map(list,lbnds[a]))
+            for i in range(3):
+                if bnds[-1][i][1] > gridSize[i]:
+                    lbnds[a][i][1] = lGridSize[i]-(bnds[-1][i][1]-gridSize[i])
+                    lbnds[-1][i][0] = lGridSize[i]-(bnds[-1][i][1]-gridSize[i])
+                    lbnds[-1][i][1] = lbnds[-1][i][0]+(bnds[-1][i][1]-gridSize[i])
+
+                    bnds[-1][i][0]  = 0
+                    bnds[-1][i][1] -= gridSize[i]
+                    bnds[a][i][1]   = gridSize[i]
+                    break
+
+                elif bnds[-1][i][0] < 0:
+                    lbnds[-1][i][0] = 0
+                    lbnds[-1][i][1] = -bnds[-1][i][0]
+                    lbnds[a][i][0] = -bnds[-1][i][0]
+
+                    bnds[-1][i][1]  = gridSize[i]
+                    bnds[-1][i][0] += gridSize[i]
+                    bnds[a][i][0]   = 0
+                    break
+
+        lfield=zeros(lGridSize)
+        for lbnd,bnd in zip(lbnds,bnds):
+            [[lxa,lxb],[lya,lyb],[lza,lzb]]=lbnd
+            [[xa,xb],[ya,yb],[za,zb]]=bnd
+            lfield[lxa:lxb, lya:lyb, lza:lzb]=field[xa:xb, ya:yb, za:zb]
+
+        #Loop over Neighboring atoms within the local grid
+        for jNeighb in jNeighbors:
+            lines[-1].append(zeros([Ninterp]))
+            atomj=latoms[jNeighb]
+            for i,aj in enumerate(atomj):
+                if aj < lGridPoints[i][0]:
+                    atomj[i]+=simSize[i]
+                if aj > lGridPoints[i][-1]:
+                    atomj[i]-=simSize[i]
+
+            #Error detection
+            if  atomj[0]>lGridPoints[0][-1] or atomj[1]>lGridPoints[1][-1] or atomj[2]>lGridPoints[2][-1] or \
+                atomj[0]<lGridPoints[0][0]  or atomj[1]<lGridPoints[1][0]  or atomj[2]<lGridPoints[2][0]:
+                d=dist_periodic(atomi,atomj,array(zip([0,0,0],simSize)))
+                if d>7.5: continue
+                print "="*50
+                print "Start Error Report:"
+                print "AtomI(%d):"%iNeighb,atomi
+                print "AtomJ(%d):"%jNeighb,atomj
+                print "Distance:",dist_periodic(atomi,atomj,array(zip([0,0,0],simSize)))
+                print "GridX Min,Max:","% 5.5f % 5.5f"%(min(lGridPoints[0]),max(lGridPoints[0]))
+                print "GridY Min,Max:","% 5.5f % 5.5f"%(min(lGridPoints[1]),max(lGridPoints[1]))
+                print "GridZ Min,Max:","% 5.5f % 5.5f"%(min(lGridPoints[2]),max(lGridPoints[2]))
+
+                print "Error: AtomJ seems to be lying outside the local grid, which should not be possible."
+                exit(0)
+            
+            #This is where the magic happens, finally do some computing...
+            #Interpolate on local FIELD grid between these two atoms 75% of time spent here
+            pnts2interp=linePoints(array([0,0,0]),atomj-atomi,Ninterp) 
+
+            vals=array([interp3d(i+atomi,lGridBoundPoints,lGridPoints,lfield) for i in pnts2interp])
+            lines[-1][-1]=vals
+            avgline+=vals
+            """
+            for j,bondCutoff in enumerate(bondCutoffs):
+                bb=False
+                for i in vals:
+                    if i<bondCutoff:
+                        cnt+=1
+                        bb=True
+                        break
+                if bb:
+                    nafs[j]+=1
+                    avgfails[j]+=array(vals)                   
+                else:
+                    naes[j]+=1
+                    eHNs[j][iNeighb].append(jNeighb)
+                    avgfields[j]+=array(vals)
+            """
+    """
+    for naf,avgfail,nae,avgfield in zip(nafs,avgfails,naes,avgfields):
+        if naf==0:
+            avgfail=zeros(len(avgfail))
+        else:
+            avgfail/=naf
+        if nae==0:
+            avgfield=zeros(len(avgfield))
+        else:
+            avgfield/=nae
+    """
+    avgline/=nlines
+    return avgline,lines,halfNeighbors
