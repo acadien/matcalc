Removing old useless calculation, fixed bits of rectify still be wary of this function as the pair correlations don't match before and after the operation.

analysis/struct_tools.py
@@ -2,7 +2,7 @@
 
 #A collection of helper functions and general utilities for analzing a set of atoms
 
-from math import *
+#from math import *
 from scipy import weave
 from scipy.weave import converters
 from numpy import *