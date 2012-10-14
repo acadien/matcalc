8559e7e1a676dac70a338093a81a8aed7bbb8a62
Adam Cadien <adamcadien@gmail.com>
2012-10-14 13:02:40 -0400

    fixed grab cluster script to work with CONTCAR/POSCARs as well as outcars

--- potfitting/grabcluster.py
+++ potfitting/grabcluster.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 #This script grabs a cluster of atoms from an OUTCAR & POSCAR.  The cluster is within a sphere whose center and radius are defined in the arguements
-import sys,thread
+import sys,threading
 from math import sqrt
+from os import path
+#mine
+from poscarIO import readposcar
+from datatools import flatten
 
-def plot3(md,ax,ay,az,afx,afy,afz,v1,v2,v3):
+def plot3(md,ax,ay,az,afx,afy,afz,v1,v2,v3,types):
+    N=len(ax)
     global thelock
-    if thelock.locked():
+    if thelock.isSet():
         print "\nPlease close all figures before continuing."
-    thelock.acquire()
+    thelock.set()
     mlab.figure()
     if md==True:
         mlab.points3d(ax,ay,az,types,colormap="gist_heat",scale_factor=0.7)
@@ -26,7 +31,9 @@ def plot3(md,ax,ay,az,afx,afy,afz,v1,v2,v3):
         ax2[2*N:]=[x-v1[0] for x in ax]
         ay2[2*N:]=[y-v2[1] for y in ay]
         az2[2*N:]=[z-v3[2] for z in az]
-        mlab.points3d(ax2[:N]*9+ax2[N:2*N]*9+ax2[2*N:]*9,(ay2[:N]*3+ay2[N:2*N]*3+ay2[2*N:]*3)*3,az2*9,types*27,colormap="gist_heat",scale_factor=0.7)
+        print len(types)*27,len(az2)*9
+        mlab.points3d(ax2[:N]*9+ax2[N:2*N]*9+ax2[2*N:]*9,(ay2[:N]*3+ay2[N:2*N]*3+ay2[2*N:]*3)*3,az2*9,[d+1.0 for d in types]*27,scale_factor=0.7)
+        #mlab.points3d(ax,ay,az,[d+1.0 for d in types],scale_factor=1.0)
         mlab.axes()
     mlab.plot3d([0,v1[0]],[0,v1[1]],[0,v1[2]],color=(0,1,0))
     mlab.plot3d([v1[0],v1[0]+v2[0]],[v1[1],v1[1]+v2[1]],[v1[2],v1[2]+v2[2]],color=(0,1,0))
@@ -43,16 +50,18 @@ def plot3(md,ax,ay,az,afx,afy,afz,v1,v2,v3):
     mlab.plot3d([v3[0],v3[0]+v2[0]],[v3[1],v3[1]+v2[1]],[v3[2],v3[2]+v2[2]],color=(0,1,0))
     mlab.plot3d([v3[0]+v1[0],v1[0]+v2[0]+v3[0]],[v3[1]+v1[1],v1[1]+v2[1]+v3[1]],[v3[2]+v1[2],v1[2]+v2[2]+v3[2]],color=(0,1,0))
 
-    mlab.show()
-    thelock.release()
+#    mlab.show(stop=True)
+#    engine=mlab.get_engine()
+    mlab.show(stop=True)
+    thelock.clear()
     return
 
 
 def plot3sphere(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr):
     global thelock
-    if thelock.locked():
+    if thelock.isSet():
         print "\n Please close all other figures before continuing."
-    thelock.acquire()
+    thelock.set()
 
     types2=list()
     for i in types:
@@ -68,11 +77,12 @@ def plot3sphere(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr):
                 types2[i]=1.75
 
     print "\nFound %d atoms inside of the sphere." % (cnt)
-
+    mlab.clf()
     if md==True:
         mlab.points3d(ax,ay,az,types2,colormap="gist_heat",scale_factor=0.7)
         mlab.quiver3d(ax,ay,az,afx,afy,afz,line_width=3,scale_factor=1)
     else:
+        mlab.points3d(ax,ay,az,types,colormap="gist_heat",scale_factor=0.7)
         ax2=[0]*N*3
         ay2=[0]*N*3
         az2=[0]*N*3
@@ -85,7 +95,7 @@ def plot3sphere(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr):
         ax2[2*N:]=[x-v1[0] for x in ax]
         ay2[2*N:]=[y-v2[1] for y in ay]
         az2[2*N:]=[z-v3[2] for z in az]
-        mlab.points3d(ax2[:N]*9+ax2[N:2*N]*9+ax2[2*N:]*9,(ay2[:N]*3+ay2[N:2*N]*3+ay2[2*N:]*3)*3,az2*9,types2*27,colormap="gist_heat",scale_factor=0.7)
+        mlab.points3d(ax2[:N]*9+ax2[N:2*N]*9+ax2[2*N:]*9,(ay2[:N]*3+ay2[N:2*N]*3+ay2[2*N:]*3)*3,az2*9,types2*27,scale_factor=0.7)
     mlab.plot3d([0,v1[0]],[0,v1[1]],[0,v1[2]],color=(0,1,0))
     mlab.plot3d([v1[0],v1[0]+v2[0]],[v1[1],v1[1]+v2[1]],[v1[2],v1[2]+v2[2]],color=(0,1,0))
     mlab.plot3d([v1[0],v1[0]+v3[0]],[v1[1],v1[1]+v3[1]],[v1[2],v1[2]+v3[2]],color=(0,1,0))
@@ -101,7 +111,7 @@ def plot3sphere(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr):
     mlab.plot3d([v3[0],v3[0]+v2[0]],[v3[1],v3[1]+v2[1]],[v3[2],v3[2]+v2[2]],color=(0,1,0))
     mlab.plot3d([v3[0]+v1[0],v1[0]+v2[0]+v3[0]],[v3[1]+v1[1],v1[1]+v2[1]+v3[1]],[v3[2]+v1[2],v1[2]+v2[2]+v3[2]],color=(0,1,0))
 
-    sphere = mlab.points3d(rx, ry, rz, scale_mode='none', scale_factor=rr*2, color=(0.67, 0.77, 0.93), resolution=50, opacity=0.7, name='Earth')
+    sphere = mlab.points3d(rx, ry, rz, scale_mode='none', scale_factor=rr*2, color=(0.67, 0.77, 0.93), resolution=50, opacity=1.0, name='Earth')
 
     # These parameters, as well as the color, where tweaked through the GUI, with the record mode to produce lines of code usable in a script.
     sphere.actor.property.specular = 0.45
@@ -111,46 +121,105 @@ def plot3sphere(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr):
 
     mlab.axes()
 
-    mlab.show()
-    thelock.release()
+    mlab.show(stop=True)
+    thelock.clear()
     return
+
+def prompt_iface(md,count,stresskb,T,PE,KE,ax,ay,az,afx,afy,afz,v1,v2,v3,types):
+    print "="*50
+    print "Step Number: %d"%(count)
+    print "Stress (in kB) XX YY ZZ XY YZ XZ:"
+    print stresskb
+    if md==True:
+        print "T=%g,   PE=%g,   TE=%g" % (T,PE,KE)
+    else:
+        print "PE=%g" % (PE)
+    print "="*50
+    print ""
+
+    if not(plotdisabled):
+        athread=threading.Thread(target=plot3,args=(md,ax,ay,az,afx,afy,afz,v1,v2,v3,types))
+        athread.start()
+        athread.join()
+    while True:
+        try:
+            val=raw_input("Input coords and radius of sphere (x y z r) or (Enter) to continue:\n")
+            [rx,ry,rz,rr]=[float(k) for k in val.split()]
+        except ValueError:
+            if len(val)==0:
+                break
+        else:
+            if not(plotdisabled):
+                bthread=threading.Thread(target=plot3sphere,args=(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr))
+                bthread.start()
+                bthread.join()
+                try:
+                    val=raw_input("Good? (y/n):")
+                    if val=="y" or val=="Y":
+
+                        N=len(ax)
+                        atoms=[[ax[i],ay[i],az[i],types[i]] for i in range(N) if sqrt((ax[i]-rx)**2+(ay[i]-ry)**2+(az[i]-rz)**2)<=rr]
+
+                        atoms.sort(key=lambda x:x[3])
+                        d1,d2,d3,types=zip(*atoms)
+                        cs=[types.count(i) for i in range(N)]
+                        #cut off types list after last non-zero number
+                        last=0
+                        for i,v in enumerate(cs):
+                            if v>0:
+                                last=i
+                        cs=cs[:last+1]
+                        print cs[:]
+                        for atom in atoms:
+                            print "% 5.5g  % 5.5g  % 5.5g" % (tuple(atom[:3]))
+                            
+                        exit(0)
+                except ValueError:
+                    pass
+
+def usage():
+    print "Usage:"
+    print "%s <directory containing OUTCAR & POSCAR, or just POSCAR file> <optional: starting config #> <optional 0: disables plotting>"%(sys.argv[0].split("/")[-1])
 
 #Reading in the options and preparing for file reading
 if len(sys.argv)<2:
-    print "Usage:"
-    print sys.argv[0]+" <directory containing OUTCAR & POSCAR> <optional: starting config #> <optional 0: disables plotting>"
+    usage()
     exit(0)
 
-outcar = open(sys.argv[1]+"/OUTCAR","r")
-poscar = open(sys.argv[1]+"/POSCAR","r") #need to use the poscar to fetch the atom types
+if path.isdir(sys.argv[1]):
+    outcar = open(sys.argv[1]+"/OUTCAR","r")
+    poscar = open(sys.argv[1]+"/POSCAR","r") #need to use the poscar to fetch the atom types
+elif path.isfile(sys.argv[1]):
+    outcar = False
+    poscar = open(sys.argv[1],"r")
 
-poscar.readline()
-poscar.readline()
-v1=map(float,poscar.readline().split())
-v2=map(float,poscar.readline().split())
-v3=map(float,poscar.readline().split())
-nums=map(int,poscar.readline().split())
-types=list()
-for i in range(len(nums)):
-    types+=[len(nums)-i]*nums[i]
-
+v1,v2,v3,atypes,ax,ay,az,head,poscar=readposcar(poscar.readlines())
+afx,afy,afz=[0]*len(ax),[0]*len(ay),[0]*len(az)
+types=[i for i in flatten([[i]*num for i,num in enumerate(atypes)])]
 startconfig=0
-disabled=False
-if (len(sys.argv)>=3):
-    startconfig=int(sys.argv[2])
-    if (len(sys.argv)>=4):
-        if int(sys.argv[3])==0:
-            disabled=True
-if not(disabled):
-    from enthought.mayavi import mlab
+plotdisabled=False
+
+if outcar:
+    if (len(sys.argv)>=3):
+        startconfig=int(sys.argv[2])
+if (len(sys.argv)>=4):
+    if int(sys.argv[3])==0:
+        plotdisabled=True
+if not(plotdisabled):
+    from mayavi import mlab
 
 global thelock
-thelock=thread.allocate_lock()
+thelock=threading.Event()
+
+if not outcar:
+    prompt_iface(False,0,0,0,0,0,ax,ay,az,afx,afy,afz,v1,v2,v3,types)
+    exit(0)
 
 N=sum(nums)
 count=0
 md=True
 started=0
+
 #File reading, plotting and grabbing
 while True:
     line=outcar.readline()
@@ -212,47 +281,5 @@ while True:
                         except IndexError:
                             md=False
                     break
-            print "="*50
-            print "Step Number: %d"%(count)
-            print "Stress (in kB) XX YY ZZ XY YZ XZ:"
-            print stresskb
-            if md==True:
-                print "T=%g,   PE=%g,   TE=%g" % (T,PE,KE)
-            else:
-                print "PE=%g" % (PE)
-            print "="*50
-            print ""
+            prompt_iface(md,count,stresskb,T,PE,KE,ax,ay,az,afx,afy,afz,v1,v2,v3,types)
 
-            if not(disabled):
-                athread=thread.start_new_thread(plot3,(md,ax,ay,az,afx,afy,afz,v1,v2,v3))
-            while True:
-                try:
-                    val=raw_input("Input coords and radius of sphere (x y z r) or (Enter) to continue: ")
-                    [rx,ry,rz,rr]=[float(k) for k in val.split()]
-                except ValueError:
-                    if len(val)==0:
-                        break
-                else:
-                    if not(disabled):
-                        bthread=thread.start_new_thread(plot3sphere,(md,ax,ay,az,afx,afy,afz,v1,v2,v3,rx,ry,rz,rr))
-                        try:
-                            val=raw_input("Good? (y/n):")
-                            if val=="y" or val=="Y":
-                                N=len(ax)
-                                type1=list()
-                                type2=list()
-                                for i in range(N):
-                                    if sqrt((ax[i]-rx)**2+(ay[i]-ry)**2+(az[i]-rz)**2)<=rr:
-                                        toprint="% 5.5g  % 5.5g  % 5.5g" % (ax[i],ay[i],az[i])
-                                        if types[i]==1:
-                                            type1.append(toprint)
-                                        else:
-                                            type2.append(toprint)
-                                print len(type2),len(type1)
-                                for i in type2:
-                                    print i
-                                for i in type1:
-                                    print i
-                                exit(0)
-                        except ValueError:
-                            pass
