0ff0547e1f0645497155798d01dc9ea97066b687
Adam Cadien <adamcadien@gmail.com>
2012-10-29 16:43:14 -0400

    script for remote plotting with mayavi libs

---
+++ plotRemoteMaya.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os
+
+#This script detects if a session is being run remotely:
+#if remote - save figure to directory BASEDIR
+#if local - plot it
+#How to use this script: import *before* importing pylab or mpl
+#use prshow(fname) instead of pylab.show()
+
+REMOTESESSION_BASEDIR="/home/acadien/Dropbox/"
+
+try:
+    os.environ['SSH_CLIENT']
+except KeyError:
+    #Local connection
+    REMOTESESSION=False
+else:
+    REMOTESESSION=True
+    from mayavi import mlab                                                     
+    mlab.options.offscreen = True
+
+
+def prmshow(fname="latestplot.png"):
+    if REMOTESESSION:
+        mlab.savefig(REMOTESESSION_BASEDIR+fname)                 
+    else:
+        mlab.show()
+
+        
