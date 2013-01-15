fecbd4eb470e3d1794f0c2202e867d3645a4eec9
Adam Cadien <adamcadien@gmail.com>
2013-01-15 15:11:12 -0500

    plot2 works for a single x/y column selection

--- analysis/plot2.py
+++ analysis/plot2.py
@@ -4,19 +4,29 @@ import sys
 import pylab as pl
 
 def usage():
-    print "%s <data-file> <x-data column> <y-data column>"%sys.argv[0].split("/")[-1]
-    print "A general use plotter, will attempt to find data in column format and plot the desired columns."
+    print "\nUsage: %s <data-file> <x-data column> <y-data column>"%sys.argv[0].split("/")[-1]
+    print "\nA general use plotter of 2D data. \nAttempts to find data in column format and plot the desired columns."
     print "If x-data column is -1 then range(len(y)) is used"
 
 if len(sys.argv)!=4:
     usage()
     exit(0)
 
+fname=sys.argv[1]
+xCol=int(sys.argv[2])
+yCol=int(sys.argv[3])
+
+fraw = open(fname,"r").readlines()
+
 data=list() 
-datanums=list()
-for i,line in enumerate(open(sys.argv[1],"r").readlines()):
+dataNum=list()
+for i,line in enumerate(fraw):
     #drop lines with only newline characters
     if len(line) < 2: 
+        continue
+
+    #drop comment lines
+    if line[0]=="#":
         continue
 
     #drop lines that don't contain columns of numbers
@@ -25,9 +35,39 @@ for i,line in enumerate(open(sys.argv[1],"r").readlines()):
         l=map(float,l)
     except ValueError:
         continue
+
     data.append(l)
-    datanums.append(i)
+    dataNum.append(i)
 
 #Keep only data with the most common number of columns
-lengths=map(len,data)
-print [lengths.count(i) for i in range(max(lengths)+1)]
+columns=map(len,data)
+colN=[columns.count(i) for i in range(max(columns)+1)]
+colN=colN.index(max(colN))
+[dataNum,data]=zip(*[[dataNum[i],d] for i,d in enumerate(data) if columns[i]==colN])
+data=zip(*data)
+
+#Error check on column selection
+if yCol >= len(data):
+    print "Error: Max column number is %d, but %d requested."%(len(data)-1,yCol)
+if xCol >= len(data):
+    print "Error: Max column number is %d, but %d requested."%(len(data)-1,xCol)
+
+#Column selection
+ydata=data[yCol]
+if xCol==-1:
+    xdata=range(len(ydata))
+else:
+    xdata=data[xCol]
+pl.plot(xdata,ydata)
+
+#Try to track down column labels
+labels=fraw[dataNum[0]-1].split()
+if len(labels)==colN:
+
+    if xCol!=-1:
+        pl.xlabel( labels[xCol] )
+        
+    pl.ylabel( labels[yCol] )
+
+pl.show()
+
