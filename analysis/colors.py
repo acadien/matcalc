4b0e50ec85cf2e7a069717cbda6e26d9dc986a0e
Adam Cadien <adamcadien@gmail.com>
2013-02-07 14:57:38 -0500

    improved RGB_all

--- analysis/colors.py
+++ analysis/colors.py
@@ -5,9 +5,9 @@ import matplotlib.colors as col
 
 def float2rgb(val, cmin, cmax):
     x = float(val-cmin)/float(cmax-cmin)
-    r = min(max(4*fabs(x-0.5)-1., 0.), 1.)
-    g = min(max(4*(x-0.25), 0.), 1.)
-    b = min(max(4*(0.75-x), 0.), 1.)
+    r = min(max(4*fabs(x-0.5)-1., 0.) +0.5, 1.)
+    g = min(max(4*(x-0.25), 0.) +0.5, 1.)
+    b = min(max(4*(0.75-x), 0.) +0.5, 1.)
     return (r,g,b)
 
 #Blue-Green-Yellow-Red-Magenta
