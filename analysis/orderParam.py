added order param, fixed database weighting

analysis/orderParam.py
@@ -1,33 +1,25 @@
 #!/usr/bin/python
+#Raw functions for calculating order parameters such as bond orientation, translational and tetrahedral ordering.
 
-from scipy import special
 import numpy as np
+from scipy.special import sph_harm as sph_harm
+from scipy import conj
 import math
 from operator import mul
 import pylab as pl
 #mine
-from neighbors import neighbors
+from neighbors import neighbors,full2half
 from struct_tools import *
 from rdf import rdf,generateRCut,rdf_periodic
-from datatools import flatten,windowAvg
 
-#Raw functions for calculating order parameters such as bond orientation, translational and tetrahedral ordering.
-
-#Helper function for bond-orientation
-#the spherical harmonics for the angle between two 3D points a and b
-def pairSphereHarms(a,b,l):
-    theta,phi=sphang(a,b)
-    return special.sph_harm(np.array(range(-l,l+1)),l,theta,phi)
-
-#bond-orientational: Q_l for each atom.  atomi>-1 selects a specific atom
+#local bond-orientational: Q_l for each atom.  atomi>-1 selects a specific atom
 def bondOrientation(atoms,basis,l,neighbs=None,rcut=None,debug=True):
-    #First make the neighbor list
+
     if neighbs==None:
         if rcut==None:
             rcut = generateRCut(atoms,debug=debug)
         bounds=[[0,basis[0][0]],[0,basis[1][1]],[0,basis[2][2]]]
         neighbs = neighbors(atoms,bounds,rcut)
-#        allatoms,reduceGhost = makeGhosts(atoms,basis)
 
     #sum the spherical harmonic over ever neighbor pair
     Qlms = [sum( [ pairSphereHarms(atoms[i],minImageAtom(atoms[i],atoms[j],basis),l) for j in ineighbs ] ) \
@@ -36,7 +28,75 @@ def bondOrientation(atoms,basis,l,neighbs=None,rcut=None,debug=True):
 
     return Ql
 
-def coordinationNumber(atoms,basis,l=None,neighbs=None,rcut=None,debug=True):
+#Helper function, returns Qlm values m=(-l .. 0 .. +l) for a specific atom pair: atomi,atomj
+def bondOrientR(atoms,basis,l,atomi,atomj):
+    ai = atoms[atomi]
+    aj = atoms[atomj]
+    Qlm = pairSphereHarms(ai,minImageAtom(ai,aj,basis),l)
+
+    return Qlm
+
+#Bond antle correlation function Gl as defined in:
+#       Nature Materials, Vol2, Nov. 2003, Sastry & Angell
+def bondAngleCorr(atoms,basis,l,neighbs=None,rcut=None,debug=False):
+    
+    print "Start Bond Angle Correlation Calculation"
+
+    if neighbs==None:
+        #if rcut==None:
+        #    rcut = generateRCut(atoms,debug=debug)
+        rcut = 10.0
+        bounds=[[0,basis[0][0]],[0,basis[1][1]],[0,basis[2][2]]]
+        #Full neighbor list
+        fneighbs = neighbors(atoms,bounds,rcut)    
+        #Half neighbor list
+        hneighbs = full2half(fneighbs)
+
+    #At distances rbins calculate the bond angle correlation function
+    nbins = 256
+    delr  = rcut/nbins
+
+    #Histogram of bond lengths
+    rbins = [i*delr for i in range(nbins)]
+    bvals = [list() for i in range(nbins)]
+    gvals = [0.0 for i in range(nbins)]
+
+    #Bond lengths
+    blens = [[ minImageDist(atoms[i],atoms[j],basis) for j in ineighbs ] \
+                 for i,ineighbs in enumerate(hneighbs)]
+
+    #Get the atomic pairs at each bond length
+    for i,ds in enumerate(blens):
+        for jj,d in enumerate(ds):
+            #i & j make an atom pair, d is the bond length between them
+            j = hneighbs[i][jj]
+            bvals[int(d/delr)]+=[(i,j)]
+
+    print "Generated Bond lengths"
+
+    #At bond length 0, Qlm has one non-zero value at m=0
+    Ql0 = conj(sph_harm(0,l,0,0))
+    Q0=bondOrientR(atoms,basis,0,0,1) #always 0.28209479 = 1/sqrt(4*pi)
+    
+    #always use m=0, due to Ql0 normalizing factor which is only non-zero at m=0.
+    norm  = 2*(l+1)*Q0*Q0
+    for b,r in enumerate(rbins):
+        pairs = bvals[b]
+
+        n=len(pairs)
+        if n>0:
+            theta,phi=sphang(atoms[i],minImageAtom(atoms[i],atoms[j],basis))
+            w = Ql0/n/norm
+            gvals[b]= (sum([special.sph_harm(0,l,theta,phi) for (i,j) in pairs])*w).real
+            print n,gvals[b]
+
+
+    print "Finished binning bond angle values"
+
+    return rbins,gvals
+
+
+def coordinationNumber(atoms,basis,l=None,neighbs=None,rcut=None,debug=False):
     #l: not used
     
     if neighbs==None:
@@ -56,14 +116,16 @@ def radialDistribution(atoms,basis,l=None,neighbs=None,rcut=None,debug=False):
      rbins,rdist = rdf_periodic(atoms,basis,cutoff=rcut)
      return rbins,rdist
 
-#translational: 
-def translational(atoms,basis,neighbs=None,rcut=None):
+#translational order parameter
+def translational(atoms,basis,l=None,neighbs=None,rcut=None,debug=False):
+    #l: not used
+
     if rcut==None:
-        rcut = generateRCut(atoms,debug=True)
+        rcut = generateRCut(atoms,debug=debug)
     r,g = rdf(atoms,cutoff=rcut)
     h=map(math.fabs,g-1)
     
     tao = sum(h)/len(r)
     return tao
         
-#tetrahedral
+

analysis/struct_tools.py
@@ -3,10 +3,11 @@
 #A collection of helper functions and general utilities for analzing a set of atoms
 
 #from math import *
-from scipy import weave
+from scipy import weave,special
 from scipy.weave import converters
 from numpy import *
 import pylab as pl
+import numpy as np
 #mine
 from datatools import flatten,windowAvg 
 
@@ -180,6 +181,11 @@ def sphang(a,b):
     if angs[1] != angs[1]: #test if phi is NaN, occurs if a and b lay on z-axis
         angs[1]=0
     return angs[0],angs[1] #theta,phi
+
+#the spherical harmonic corresponding to two atoms a and b
+def pairSphereHarms(a,b,l):
+    theta,phi=sphang(a,b)
+    return special.sph_harm(np.array(range(-l,l+1)),l,theta,phi)
 
 bounds_code="""
 double d=0.0;