9b71c417a7f2d8e536a8d719192357ed190d3ea5
Adam Cadien <adamcadien@gmail.com>
2013-08-15 10:18:50 -0400

    improved arguement handling

--- moldyn/dump2poscar.py
+++ moldyn/dump2poscar.py
@@ -7,7 +7,15 @@ import lammpsIO
 
 def usage():
     print "%s <lammps dump> <config #> <POSCAR output>"%sys.argv[0].split("/")[-1]
+    print "If only the dump file is provided, the number of configurations will be reported"
     print "Converts a lammps dump to POSCAR"
+
+#If only the filename is provided, count the number of configurations and spit that out
+if len(sys.argv)==2:
+    dumpdata=open(sys.argv[1],"r").readlines()
+    usage()
+    print "\nNumber of configurations:",len([i for i in dumpdata if "ITEM: TIMESTEP" in i]),"\n"
+    exit(0)    
 
 if len(sys.argv)!=4:
     usage()
