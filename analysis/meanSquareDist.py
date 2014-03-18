fixed msd calculation, account for PBC now

analysis/meanSquareDist.py
@@ -4,7 +4,6 @@ from scipy import weave
 from scipy.weave import converters
 from numpy import *
 
-#Yeah that's right stick your code in a fucking comment cause its a weave hack.
 msdCode = """
 double c,d;
 double *seta,*setb;
@@ -49,6 +48,86 @@ def meanSquareDist(atoms,Natom,Ntime,lengths):
     libs=['gomp']
 
     weave.inline(msdCode,['atoms','Ntime','Natom','lengths','msd'],\
+                     extra_compile_args=compiler_args,\
+                     support_code=headers,\
+                     libraries=libs)
+    return delT,msd
+
+undoPBCcode = """
+int index;
+double d;
+double *seta,*setb;
+
+#pragma omp parallel for default(shared) private(index,d)
+//first you need to unwrap the periodic boundary conditions in certain cases to ensure continuity
+for(int i=0;i<Ntime-1;i++){
+
+  seta=&(atoms[i*Natom*3]);
+  setb=&(atoms[(i+1)*Natom*3]);
+
+  for(int j=0;j<Natom;j++){
+    for(int dof=0;dof<3;dof++){       //loop over each Degree of Freedom
+      index=j*3+dof;
+      d = seta[index]-setb[index];
+      if( d > lengths[dof]/2.0 ) setb[index] += lengths[dof];  //Lower PBC
+      if( d <-lengths[dof]/2.0 ) setb[index] -= lengths[dof];  //Upper PBC
+    }
+  }
+} 
+
+"""
+
+msdRefCode = """
+double c,d;
+double *seta,*setb;
+
+seta=&(refAtoms[0]);
+
+#pragma omp parallel for default(shared) private(c,d)
+for(int i=0;i<Ntime;i++){
+
+  setb=&(atoms[(i)*Natom*3]);
+
+  c=0.0;
+  for(int j=0;j<Natom;j++){
+    for(int dof=0;dof<3;dof++){       //loop over each Degree of Freedom
+
+      d = seta[j*3+dof]-setb[j*3+dof];
+      //if( d > lengths[dof]/2.0 ) d -= lengths[dof];  //Lower PBC
+      //if( d <-lengths[dof]/2.0 ) d += lengths[dof];  //Upper PBC
+
+      c += d*d;
+
+    }
+  }
+
+  msd[i] = c/(Natom*Ntime);
+
+}
+"""
+
+#Calculates the MSD enforcing periodic boundary conditions
+def meanSquareDistRef(atoms,ref,Natom,Ntime,lengths):
+
+    atoms=array(atoms).ravel()       #atoms is an Ntime x Natom x 3 array... now flattened
+    delT=array(range(1,Ntime+1))
+    msd=zeros(len(delT))
+
+    compiler_args=['-march=native -O3 -fopenmp']
+    headers=r"""#include <omp.h>"""
+    libs=['gomp']
+
+    weave.inline(undoPBCcode,['atoms','Ntime','Natom','lengths','msd'],\
+                     extra_compile_args=compiler_args,\
+                     support_code=headers,\
+                     libraries=libs)
+
+    #do this after undoing PBC to ensure refAtoms are in same state as atoms set.
+    atoms.shape=(Ntime,Natom,3)
+    refAtoms=atoms[ref] #refAtoms is an Natom x 3 array... now flattened
+    atom=atoms.ravel()
+    refAtoms=refAtoms.ravel()
+    weave.inline(msdRefCode,['atoms','refAtoms','Ntime','Natom','lengths','msd'],\
                      extra_compile_args=compiler_args,\
                      support_code=headers,\
                      libraries=libs)