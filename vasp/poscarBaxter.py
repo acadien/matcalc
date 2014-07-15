42972b1c96805d64b631d9b5338fb2fb9dce1b81
Adam Cadien <adamcadien@gmail.com>
2014-07-15 14:53:39 -0400

    so close, just get Sq working

--- vasp/poscarBaxter.py
+++ vasp/poscarBaxter.py
@@ -8,8 +8,9 @@ from scipy.signal import cspline1d,cspline1d_eval
 from scipy.integrate import cumtrapz,simps
 from scipy.optimize import leastsq
 from numpy import array,dot,cross,pi,floor
+import numpy as np
 from datatools import superSmooth,wsmooth
-
+import random
 import pylab as pl
 
 def deriv(xs,h):
@@ -23,8 +24,26 @@ def interp(dx,knots,xis):
 
 #calculate h(r) from c(r)
 def hrc_calc(dr,cr,hr,rads):
-    ihc = array([simps([hr[abs(floor((r-t)/dr))]*cr[it] for it,t in enumerate(rads)],x=rads) for ir,r in enumerate(rads)])
-    return cr+ihc*density
+    ihc = array([simps([hr[abs(ir-it)]*cr[it] for it,t in enumerate(rads)],x=rads) for ir,r in enumerate(rads)])
+    return cr + ihc*density
+
+def residual_crhr(coefs,y,x):
+    coefs = array([coefs[i] for i in range(len(coefs)) if i*dx<=Rc] + [0 for i in range(len(coefs)) if i*dx>Rc])
+    cr = interp(dx,coefs,x)
+    hr = y
+    hrc = hrc_calc(dr,cr,hr,x)
+    """
+    print sum((hr-hrc)**2)
+    import pylab as pl
+    pl.plot(hrc)
+    pl.plot(hr)
+    xknots=[i for i in range(len(coefs))]
+    pl.scatter(xknots,yknots_original,color="red")
+    pl.scatter(xknots,coefs)
+    pl.show()
+    """
+    return hr-hrc
+
 
 #calculate h(r) from Q(r)
 def hrq_calc(dr,Q,hr,rads):
@@ -50,51 +69,66 @@ def crq_calc(dr,Q,rads):
 
     return (-dQ + 2.0*pi*density*iQ)/rads
 
-
-def sq_calc(cr):
-    return
+def sq_calc(rads,cr,qs):
+    dx = rads[1]-rads[0]
+    cq = array([ 4*pi*simps(rads*cr*np.sin(q*rads)/q,dx=dx) for q in qs])
+    return 1./(1. - density*cq)
 
 def residual_qhr(coefs,y,x):
     Q = interp(dx,coefs,x)
     hr = y
     return x*hr - hrq_calc(dr,Q,hr,x)
 
-def residual_crhr(coefs,y,x):
-    cr = interp(dx,coefs,x)
-    hr = y
-    return hr-hrc_calc(dr,cr,hr,x)
 
 basis,atypes,atoms,head,poscar = poscarIO.read(open(sys.argv[1]).readlines())
 atoms=array(atoms)
 basis=array(basis)
 rads,gr = radialDistribution(atoms,basis)
-hr = wsmooth(gr) - 1#superSmooth(rads,gr)[:-20] - 1
+rads = array(rads)
+
+hr = wsmooth(gr)[:-1] - 1#superSmooth(rads,gr)[:-20] - 1
 #hr = gr - 1
 dr=rads[1]-rads[0]
-density = dot(cross(basis[0],basis[1]),basis[2])/atoms.shape[0]
+density = atoms.shape[0]/dot(cross(basis[0],basis[1]),basis[2])
 
 rcut = rads[-1]
 radsCut = [i for i in rads if i <= rcut]
 hrCut = [hr[i] for i in range(len(rads)) if rads[i] <= rcut]
 
-Nknots=20
+Nknots=70
 xknots=array([i*max(radsCut)/Nknots for i in range(Nknots)])
 dx=xknots[1]-xknots[0]
-yknots=array([i for i in range(Nknots)])
+yknots=list()
+for i in range(Nknots):
+    if i==0:
+        yknots.append(0)
+    else:
+        yknots.append(yknots[-1]*0.9+0.1*random.random()-0.05)
+yknots=array(yknots)
+#yknots=array([-0.5+random.random() for i in range(Nknots)])
+#yknots=array([i for i in range(Nknots)])
+
+Rc = 5.0
 
 #(lsq,success) = leastsq(residual_qhr,yknots,args=(hr,rads),maxfev=100)
 #Q  = interp(dx,lsq,rads)
 #cr = crq_calc(dr,Q,rads)
 #hr_new = hrq_calc(dr,Q,hr,rads)
-
-(lsq,success) = leastsq(residual_crhr,yknots,args=(hr,rads),maxfev=100)
+yknots_original = array(list(yknots))
+(lsq,success) = leastsq(residual_crhr,yknots,args=(hr,rads),maxfev=1)
 cr = interp(dx,lsq,rads)
 hr_new = hrc_calc(dr,cr,hr,rads)
 
+Nq = 1000
+qMax = 12.0
+qs = array([q/float(Nq)*qMax for q in range(Nq)])
+sq = sq_calc(rads,cr,qs)
+
 import pylab as pl
-pl.plot(rads,hr)
-pl.plot(rads,hr_new)
-pl.plot(rads,cr)
-pl.plot(rads,deriv(cr,dr))
-pl.plot([0,rads[-1]],[0,0])
+pl.plot(rads,hr,label="hrold")
+pl.plot(rads,hr_new,label="hrnew")
+pl.plot(rads,cr,label="cr")
+pl.plot(qs,sq)
+#pl.plot(rads,deriv(cr,dr))
+#pl.plot([0,rads[-1]],[0,0])
 pl.show()
